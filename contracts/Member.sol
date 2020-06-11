ba9a374c175aac75c06342bcd3afa7857f06f8ab
Maria-Cruz Valiente <mcvaliente@ucm.es>
2020-06-11 18:53:56 +0200

    Updating contract for Mocha errors in length of contract.

--- contracts/Member.sol
+++ contracts/Member.sol
@@ -2,10 +2,6 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 contract Member {
-    
-    ////////////////////////////////////////////////
-    //MEMBER SECTION
-    ////////////////////////////////////////////////
     struct MemberInfo {
         string name;
         string surname;
@@ -15,7 +11,7 @@ contract Member {
         mapping (uint => string) occupations;
         bool exists;
     }
-    
+
     struct MemberLocation {
         string office;
         string county;
@@ -25,7 +21,6 @@ contract Member {
     mapping (bytes32 => MemberInfo) public members;
     mapping (bytes32 => MemberLocation) public membersLocation;
     bytes32[] public memberList;
-    
     modifier existing(bytes32 memberId){
         require(
             members[memberId].exists,
@@ -33,12 +28,6 @@ contract Member {
         );
         _;
     }
-    
-    ////////////////////////////////////////////////
-    //GRANTED USERS SECTION
-    ////////////////////////////////////////////////
-
-    //Address that deploys the contract (i.e., the owner of the contract).
     address public owner;
     mapping (address => bool) public grantedUsers;
 
@@ -62,14 +51,13 @@ contract Member {
         owner = msg.sender;
     }
 
-    function createMember(bytes32 memberId, string memory name, string memory surname, 
-                        bytes32 birthdate, bytes32 acceptanceDate) public restricted (msg.sender) {
-    
+    function createMember(bytes32 memberId, string memory name, string memory surname,
+                            bytes32 birthdate, bytes32 acceptanceDate) public restricted (msg.sender) {
         //Check the member doesn't exist.
         require(!members[memberId].exists,
                 "ERROR_MEMBERID_EXISTS"
         );
-        
+
         MemberInfo memory newMember = MemberInfo({
             name: name,
             surname: surname,
@@ -79,33 +67,27 @@ contract Member {
             exists: true
         });
         members[memberId] = newMember;
-        
-        //Since we cannot manage members as a List, we need the next property to get to total number of members.
         memberList.push(memberId);
-        
     }
-    
     function addMemberOccupation(bytes32 memberId, string memory occupation) public existing(memberId) restricted (msg.sender) {
         uint occupationNumber = members[memberId].totalOccupations;
         members[memberId].occupations[occupationNumber] = occupation;
         members[memberId].totalOccupations = occupationNumber + 1;
     }
-    
-    function addMemberLocation(bytes32 memberId, string memory county, string memory office, string memory country) public existing(memberId) restricted (msg.sender) {
-    
+    function addMemberLocation(bytes32 memberId, string memory county,
+                                string memory office, string memory country) public existing(memberId) restricted (msg.sender)
+    {
         MemberLocation memory newMemberLocation = MemberLocation({
             county: county,
             office: office,
             country:country
         });
         membersLocation[memberId] = newMemberLocation;
-        
     }
 
     function getMembersCount() public view returns (uint) {
         return memberList.length;
     }
-    
     function getMemberOccupation(bytes32 memberId, uint occupationIndex) public existing(memberId) view returns (string memory) {
         return members[memberId].occupations[occupationIndex];
     }
@@ -117,15 +99,11 @@ contract Member {
             membersLocation[memberId].country
         );
     }
-    
-    //These functions will be invoked only by the creator of the contract and it is cannot
-    //be an option of the SmartCoopChain's DApp for adding new members.
     function addGrantedUser(address userAccount) public  onlyOwner (msg.sender){
         grantedUsers[userAccount] = true;
-    }    
-    
+    }
+
     function revokeGrantedUser(address userAccount) public onlyOwner (msg.sender) {
         grantedUsers[userAccount] = false;
     }
-    
 }
--- test/Member.test.js
+++ test/Member.test.js
@@ -4,41 +4,37 @@ const Web3 = require('web3');
 const provider = ganache.provider();
 const web3 = new Web3(provider);
 //compiled versions of the contracts
-const compiledMember = require('../src/contracts/Member.json');
-
+const compiledMember = require('../src/contracts/build/Member.json');
 
 let accounts; //List of 10 accounts provided by Ganache.
-let memberAddress;
-let member; //our contract: Member
 
 beforeEach( async () => {
 	//Get a list of all accounts
-    accounts = await web3.eth.getAccounts();
-    console.log("Available accounts: " + accounts);
+	accounts = await web3.eth.getAccounts();
+	console.log("Available accounts: " + accounts);
 	console.log(JSON.stringify(compiledMember.abi));
+	
+	//MEMBER
+	//Use one of those accounts to deploy the Member contract and get the instance.
+    member = await new web3.eth.Contract(compiledMember.abi)
+        .deploy({data: '0x' + compiledMember.evm.bytecode.object})
+        .send({from: accounts[0], gas: '1000000'});
 
-
-	//CAMPAIGN FACTORY
-	//Use one of those accounts to deploy the CampaignFactory contract and get the instance.
-	member = await new web3.eth.Contract(JSON.parse(compiledMember.abi))
-		.deploy({data: '0x' + compiledMember.evm.bytecode.object})
-        .send ({ from: accounts[0], gas: '1000000'})
-    
-    memberAddress = member.options.address;
-
-	await factory.methods.createCampaign('100').send({
-		from: accounts[0],
-		gas: '1000000'
-	});
 
 	member.setProvider(provider);
+
+
 });
 
-describe('Members',() => {
-	it('deploys a member', () => {
-		assert.ok(memberAddress);
+describe('Campaigns',() => {
+
+	it('deploys Member contract', () => {
+		//assert.ok(factory.options.address);
 		console.log('==================================================================');
-		console.log('Member address: ' + memberAddress);
+		console.log('Campaign address: ');
 		console.log('==================================================================');
+		//assert.ok(campaign.options.address);
 	});
+
+
 });
