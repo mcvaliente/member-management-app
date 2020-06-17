41c77115e31a756603a4dfb672ede408a65d8d42
Maria-Cruz Valiente <mcvaliente@ucm.es>
2020-06-17 16:46:17 +0200

    Cleaning code

--- contracts/Member.sol
+++ contracts/Member.sol
@@ -1,51 +1,34 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.4.22 <0.7.0;
 
-contract Member {
+contract MemberFactory {
 
-    ////////////////////////////////////////////////
-    //MEMBER SECTION
-    ////////////////////////////////////////////////
-    struct MemberInfo {
-        string name;
-        string surname;
-        bytes32 birthdate; //dd/mm/aaaa
-        bytes32 acceptanceDate; //dd/mm/aaaa
-        uint totalOccupations;
-        mapping (uint => string) occupations;
-        bool exists;
+    Member[] private deployedMembers;
+
+    function createMember() public {
+        Member newMember = new Member(msg.sender);
+        deployedMembers.push(newMember);
     }
 
-    struct MemberLocation {
-        string office;
-        string county;
-        string country;
+    function getDeployedMembers() public view returns (Member[] memory) {
+        return deployedMembers;
     }
 
-    mapping (bytes32 => MemberInfo) public members;
-    mapping (bytes32 => MemberLocation) public membersLocation;
-    bytes32[] public memberList;
-
-    modifier existing(bytes32 memberId){
-        require(
-            members[memberId].exists,
-            "ERROR_MEMBER_NOT_FOUND."
-        );
-        _;
+    function getMemberCount() public view returns (uint){
+        return deployedMembers.length;
     }
+}
 
-    ////////////////////////////////////////////////
-    //GRANTED USERS SECTION
-    ////////////////////////////////////////////////
+contract Ownable {
 
     //Address that deploys the contract (i.e., the owner of the contract).
-    address public owner;
-    mapping (address => bool) public grantedUsers;
+    address owner;
+    mapping (address => bool) grantedUsers;
 
     modifier onlyOwner(address userAccount){
         require(
             owner == userAccount,
-            "ERROR_UNATHORIZED_ADMIN_TASK."
+            "ERROR_NOT_OWNER."
         );
         _;
     }
@@ -53,79 +36,101 @@ contract Member {
     modifier restricted(address userAccount){
         require(
             grantedUsers[userAccount],
-            "ERROR_UNATHORIZED_USER_OPERATION."
+            "ERROR_NOT_GRANTED_USER."
         );
         _;
     }
 
-    constructor () public{
-        owner = msg.sender;
-    }
-
-    function createMember(bytes32 memberId, string memory name, string memory surname,
-                            bytes32 birthdate, bytes32 acceptanceDate) public restricted (msg.sender) {
-
-        //Check the member doesn't exist.
-        require(!members[memberId].exists,
-                "ERROR_MEMBERID_EXISTS"
-        );
-
-        MemberInfo memory newMember = MemberInfo({
-            name: name,
-            surname: surname,
-            birthdate: birthdate,
-            acceptanceDate: acceptanceDate,
-            totalOccupations: 0,
-            exists: true
-        });
-        members[memberId] = newMember;
-
-        //Since we cannot manage members as a List, we need the next property to get to total number of members.
-        memberList.push(memberId);
-
-    }
-
-    function addMemberOccupation(bytes32 memberId, string memory occupation) public existing(memberId) restricted (msg.sender) {
-        uint occupationNumber = members[memberId].totalOccupations;
-        members[memberId].occupations[occupationNumber] = occupation;
-        members[memberId].totalOccupations = occupationNumber + 1;
-    }
-
-    function addMemberLocation(bytes32 memberId, string memory county, string memory office, string memory country)
-                                public existing(memberId) restricted (msg.sender) {
-
-        MemberLocation memory newMemberLocation = MemberLocation({
-            county: county,
-            office: office,
-            country:country
-        });
-        membersLocation[memberId] = newMemberLocation;
-    }
-
-    function getMembersCount() public view returns (uint) {
-        return memberList.length;
-    }
-
-    function getMemberOccupation(bytes32 memberId, uint occupationIndex) public existing(memberId) view returns (string memory) {
-        return members[memberId].occupations[occupationIndex];
-    }
-
-    function getMemberLocation(bytes32 memberId) public existing(memberId) view returns (string memory, string memory, string memory ) {
-        return (
-            membersLocation[memberId].office,
-            membersLocation[memberId].county,
-            membersLocation[memberId].country
-        );
-    }
-
-    //These functions will be invoked only by the creator of the contract and it is cannot
-    //be an option of the SmartCoopChain's DApp for adding new members.
-    function addGrantedUser(address userAccount) public  onlyOwner (msg.sender){
+    function addGrantedUser(address userAccount) public  onlyOwner(msg.sender) {
         grantedUsers[userAccount] = true;
     }
 
-    function revokeGrantedUser(address userAccount) public onlyOwner (msg.sender) {
+    function revokeGrantedUser(address userAccount) public onlyOwner(msg.sender) {
         grantedUsers[userAccount] = false;
     }
 
 }
+
+contract Member is Ownable {
+
+    struct BasicInfo {
+        string memberId;
+        string name;
+        string surname;
+        string birthdate; //dd/mm/aaaa
+        string acceptanceDate; //dd/mm/aaaa
+        uint totalOccupations;
+        mapping (uint => string) occupations;
+    }
+
+    struct LocationInfo {
+        string office;
+        string county;
+        string country;
+    }
+
+    BasicInfo memberInfo;
+    LocationInfo memberLocation;
+
+    constructor (address creator) public{
+        owner = creator;
+    }
+
+    function addMemberBasicInformation(string memory memberId, string memory name, string memory surname,
+                            string memory birthdate, string memory acceptanceDate) public restricted(msg.sender) {
+        BasicInfo memory newMember = BasicInfo({
+            memberId: memberId,
+            name: name,
+            surname: surname,
+            birthdate: birthdate,
+            acceptanceDate: acceptanceDate,
+            totalOccupations: 0
+        });
+
+        memberInfo = newMember;
+    }
+
+    function addMemberOccupation(string memory occupation) public restricted (msg.sender) {
+        uint occupationNumber = memberInfo.totalOccupations;
+        memberInfo.occupations[occupationNumber] = occupation;
+        memberInfo.totalOccupations = occupationNumber + 1;
+    }
+
+    function addMemberLocation(string memory county, string memory office, string memory country) public restricted (msg.sender) {
+
+        LocationInfo memory newMemberLocation = LocationInfo({
+            county: county,
+            office: office,
+            country:country
+        });
+        memberLocation = newMemberLocation;
+    }
+
+    function getMemberSummary() public view returns(string memory, string memory,
+                                                    string memory, string memory, string memory, uint)
+    {
+
+        return (
+            memberInfo.memberId,
+            memberInfo.name,
+            memberInfo.surname,
+            memberInfo.birthdate,
+            memberInfo.acceptanceDate,
+            memberInfo.totalOccupations
+        );
+    }
+
+    function getMemberOccupation(uint occupationIndex) public view returns (string memory) {
+        return memberInfo.occupations[occupationIndex];
+    }
+
+    function getMemberLocation() public view returns (string memory, string memory, string memory ) {
+        return (
+            memberLocation.office,
+            memberLocation.county,
+            memberLocation.country
+        );
+    }
+
+}
+
--- test/Member.test.js
+++ test/Member.test.js
@@ -3,43 +3,63 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const provider = ganache.provider();
 const web3 = new Web3(provider);
-//compiled versions of the contract
-const compiledMember = require('../src/contracts/Artifacts.json');
+//compiled versions of the contracts
+const compiledFactory = require('../src/contracts/build/MemberFactory.json');
+const compiledMember = require('../src/contracts/build/Member.json');
 
 
 let accounts; //List of 10 accounts provided by Ganache.
-let memberContract; //Our contract: Member
+let factory; //Our contract: MemberFactory
+let memberAddress;
+let member; //our contract: Member
 
 beforeEach( async () => {
 	//Get a list of all accounts
 	accounts = await web3.eth.getAccounts();
 	console.log("Available accounts: " + accounts);
+	//console.log(JSON.stringify(compiledFactory.abi));
 	
-	//MEMBER
-	//The deployment address of the Member.sol contract that we obtained in Remix.
-	const address = "0xe085a0a0b36dA3adbBC98d1371c8AA7d132834c4";
-	const from = accounts[0];
-	const options = { from };
-	//Use one of those accounts to get the instance.
-	const name = "contracts/Member.sol:Member";
-	const artifact = compiledMember.contracts[name];
-	const abi = JSON.parse(artifact.abi);
-	membercontract = new web3.eth.Contract(abi, address, options);
-  
-	//memberContract.setProvider(provider);
+	//MEMBER FACTORY
+	//Use one of those accounts to deploy the MemberFactory contract and get the instance.
+    factory = await new web3.eth.Contract(compiledFactory.abi)
+        .deploy({data: '0x' + compiledFactory.evm.bytecode.object})
+        .send({from: accounts[0], gas: '1000000'});
+
+	//await factory.methods.createCampaign('100').send({
+	//	from: accounts[0],
+	//	gas: '1000000'
+	//});
+
+	//CAMPAIGN
+	//const addresses = await factory.methods.getDeployedCampaigns().call();
+	//campaignAddress = addresses[0];
+	//Javascript representation of the Campaign instance that we have created through the campaign contract address.
+	//campaign = await new web3.eth.Contract(compiledCampaign.abi, campaignAddress);
+
+	//factory.setProvider(provider);
+	//member.setProvider(provider);
 
 
 });
 
-describe('Member Management',() => {
+describe('Members',() => {
 
-	it('Get Member contract', () => {
-		//assert.ok(factory.options.address);
+	it('deploys a factory and a member', () => {
+		assert.ok(factory.options.address);
 		console.log('==================================================================');
-		console.log('Member address: ');
+		console.log('Member factory address: ' + factory.options.address);
 		console.log('==================================================================');
 		//assert.ok(campaign.options.address);
 	});
 
+	it('grants an address in order to add new members', async () => {
+        await member.methods.addGrantedUser(accounts[1]).send({
+            from: accounts[0],
+            gas: '1000000'
+        });
+
+        const isGrantedUser = await member.methods.grantedUsers(accounts[1]).call(); //return true if it is recorded as a granted user.
+        assert(isGrantedUser);
+    });
 
 });
