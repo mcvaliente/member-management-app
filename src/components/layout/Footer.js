209b3d17ed4a0360ccebc8c69cc58ec54adc484f
mcvaliente <mcvaliente@ucm.es>
2020-12-21 18:38:20 +0100

    resolving #5

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import styles from '../../assets/css/Footer.module.css'
-import { Image } from 'semantic-ui-react';
 
 
 export default () => {
     const srcGitHubImg = '/images/github-icon.svg';
     
     return(
-        <div className = {styles.Footer}>
-            <a
-                className={styles.FooterLink}
-                href="https://p2pmodels.eu/"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{marginLeft : 50}}>
-                P2PModels
-            </a>
-            <span>, 2020. </span>
-            <div className={styles.FooterInnerDiv}>
-                <Image src={srcGitHubImg} avatar style={{marginRight: '10px'}} />
+        <div className = {styles.Footer}>            
+            <span>
+                <a
+                    className={styles.FooterLink}
+                    href="https://p2pmodels.eu/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    P2PModels
+                </a>
+                , 2020. 
+            </span>
+            <div style={{ display: "flex", justifyContent: 'center', alignItems: "center"}}>
+                <img src={srcGitHubImg} alt=""/>
                 <a
                     className={styles.FooterLink}
                     href="https://github.com/P2PModels"
                     target="_blank"
-                    rel="noopener noreferrer">
-                 GitHub
+                    rel="noopener noreferrer"
+                    style={{marginLeft: '10px'}}>
+                    GitHub
                 </a>
             </div>
-
         </div>
     );
 };
